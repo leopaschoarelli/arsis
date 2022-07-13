0018206f2411532983c4aa308affeaaf5fbf8a1d
leo95h <leonardo.lhpr@gmail.com>
2022-07-13 16:13:42 -0300

    Project created

---
+++ backend/salamusical/src/main/java/br/com/arsis/salamusical/SalamusicalApplication.java
@@ -0,0 +1,13 @@
+package br.com.arsis.salamusical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalamusicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalamusicalApplication.class, args);
+	}
+
+}
---
+++ backend/salamusical/src/test/java/br/com/arsis/salamusical/SalamusicalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arsis.salamusical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalamusicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
