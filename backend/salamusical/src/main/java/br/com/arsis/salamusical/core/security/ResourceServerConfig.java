0146b030d4fb5d4dd4f61a53800135119df119f5
leo95h <leonardo.lhpr@gmail.com>
2022-07-14 13:44:07 -0300

    Alterando para token JWT

--- backend/salamusical/src/main/java/br/com/arsis/salamusical/core/security/ResourceServerConfig.java
+++ backend/salamusical/src/main/java/br/com/arsis/salamusical/core/security/ResourceServerConfig.java
@@ -1,9 +1,14 @@
 package br.com.arsis.salamusical.core.security;
 
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @Configuration
 @EnableWebSecurity
@@ -15,7 +20,15 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 		    .authorizeRequests()
 		        .anyRequest().authenticated()
 		    .and()
-		    .oauth2ResourceServer().opaqueToken();
+		    .oauth2ResourceServer().jwt();
+	}
+	
+	@Bean
+	public JwtDecoder jwtDecoder() {
+		
+		var secretKey = new SecretKeySpec("arsis".getBytes(), "HmacSHA256");
+		
+		return NimbusJwtDecoder.withSecretKey(secretKey).build();
 	}
 
 }
