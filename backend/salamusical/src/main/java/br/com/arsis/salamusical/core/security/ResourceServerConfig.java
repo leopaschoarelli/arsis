6c78d5b435992e3486821fb0742853419d9c31ba
leo95h <leonardo.lhpr@gmail.com>
2022-07-14 14:28:53 -0300

    Alterando JWT para chave publica atraves da chave privada JKS

--- backend/salamusical/src/main/java/br/com/arsis/salamusical/core/security/ResourceServerConfig.java
+++ backend/salamusical/src/main/java/br/com/arsis/salamusical/core/security/ResourceServerConfig.java
@@ -1,14 +1,9 @@
 package br.com.arsis.salamusical.core.security;
 
-import javax.crypto.spec.SecretKeySpec;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @Configuration
 @EnableWebSecurity
@@ -20,15 +15,8 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 		    .authorizeRequests()
 		        .anyRequest().authenticated()
 		    .and()
+		    .cors().and()
 		    .oauth2ResourceServer().jwt();
 	}
-	
-	@Bean
-	public JwtDecoder jwtDecoder() {
 		
-		var secretKey = new SecretKeySpec("arsis".getBytes(), "HmacSHA256");
-		
-		return NimbusJwtDecoder.withSecretKey(secretKey).build();
-	}
-
 }
