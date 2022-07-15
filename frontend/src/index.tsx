95fe17c0624da4b6637b99423073dd2a59e953cc
leo95h <leonardo.lhpr@gmail.com>
2022-07-15 19:17:44 -0300

    Iniciando frontend, tela de login

---
+++ frontend/src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import LoginPage from './LoginPage';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <LoginPage />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,8 @@
+function LoginPage() {
+  return (
+    <h1>TESTE
+    </h1>
+  );
+}
+
+export default LoginPage;
